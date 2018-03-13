9c393498c00418870dc1a572416ebcc1be4127ca
Ali Ebner <Ali@Alis-MacBook-Air.local>
2018-03-13 14:59:43 -0500

    coffeeShop object

--- coffeeShopHopper.js
+++ coffeeShopHopper.js
@@ -1,9 +1,21 @@
-function coffeeShop(name,roasters,machine,year,rating){
+function coffeeShop(name,locations,roasters,machine,year,rating){
   this.name = name;
+  this.locations = locations;
   this.roasters = roasters;
   this.machine = machine;
   this.year = year;
+  this.getAge = function(){
+    return Date.now().getFullYear() - this.year;
+  }
   this.rating = rating;
+
 }
 
-var medici = new coffeeShop("Caffé Medici", ["Spyhouse", "Wild Gift Coffee", "Rotating Guest Roasters"], "La Marzocco", "Established in 2006", 4.6);
+function oldestShop(shop1, shop2, ...arguments){
+  for(let i=0;i<arguments.length;i++){
+    return Math.min.apply(null,arguments);
+  }
+}
+
+var medici = new coffeeShop("Caffé Medici", ["West Lynn", "Guadalupe", "Austonian", "South Lamar", "8C"],["Spyhouse", "Wild Gift Coffee", "Rotating Guest Roasters"], "La Marzocco", "Established in 2006", 4.6);
+var houndstooth = new coffeeShop("Houndstooth Coffee", ["North Lamar", "Frost", "MLK"], ["Tweed"], "Strada", "Established in 2009", 4.7);
